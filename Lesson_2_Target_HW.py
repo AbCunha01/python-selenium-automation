acb27d3592fac85acbadb456dd263c4b0f1251dc
christiane cunha <AriCunha71@gmail.com>
2024-12-07 18:15:04 -0500

    Lesson_2_Target_HW.py

--- Lesson_2_Target_HW.py
+++ Lesson_2_Target_HW.py
@@ -8,7 +8,6 @@ from time import sleep  # Optional, use WebDriverWait instead for a cleaner appr
 
 driver_path = ChromeDriverManager().install()
 
-# Create Chrome browser instance (service object recommended)
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
@@ -19,8 +18,9 @@ driver.get("https://www.target.com/")
 
 try:
 
-  WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//use[@href="/icons/Account.svg#Account"]')))
-  driver.find_element(By.XPATH,'//a[@data-test="accountNav-signIn"]').click()
+  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//use[@href="/icons/Account.svg#Account"]')))
+  driver.find_element(By.XPATH,"//a[text()='Sign in']").click()
+
 except:
   pass
 
