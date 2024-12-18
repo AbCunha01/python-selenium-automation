df3806d8f03f5c97338d512bd28bce6c10f6bf94
git AbCunha01 <AriCunha71@gmail.com>
2024-12-17 22:28:58 -0500

    HW3_Amazon_Locators.py

--- features/tests/Target_Lesson3_HW.py
+++ features/tests/Target_Lesson3_HW.py
@@ -17,6 +17,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
+#Feature: Verify Empty Cart Message
 @given("I am on the Target homepage")
 def open_main(context):
     context.driver.get('https://www.target.com/')
@@ -33,21 +34,17 @@ def open_main(context):
         assert empty_message == "Your cart is empty"
 
 
-    @after_scenario
-    def close_browser(context):
-        context.driver.quit()
-
 # Update these steps
 def click_cart_icon(context):
   cart_icon = context.driver.find_element(By.CSS_SELECTOR('Cart.svg#Cart')).click()
 
-
+@When("I click 'Sign In' from the navigation menu")
 def click_sign_in_nav(context):
-    sign_in_link = WebDriverWait(context.driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "[title='Sign in']"))
+    sign_in_link = WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "[title='Sign in']"))
     )
     sign_in_link.click()
 
+@Then("Verify that the 'Sign In' form opened")
 def verify_sign_in_form(context):
   sign_in_form = context.driver.find_element(By.ID("Sign in"))
   assert sign_in_form.is_displayed()
